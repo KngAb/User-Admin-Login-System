3ca68c798f46c26607c04b6e35cda1dd8ef92bec
KngAb <122820263+KngAb@users.noreply.github.com>
2024-06-11 08:15:20 +0100

    Update and rename index.php to login_pro.php

--- login_pro.php
+++ login_pro.php
@@ -31,41 +31,4 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<center>
-    <h1>Login Form</h1>
-    <br>
-    <br>
-    <br>
-    <br>
-    <div style="background-color: grey; width: 500px;">
-    <br>
-    <br>
-    <form action='#' method="POST">
-    <div>
-        <label>username</label>
-        <input type="text" name="username" require>
-    </div>
-    <br><br>
-    <div>
-        <label>password</label>
-        <input type="password" name="password" require>
-    </div>
-    <br><br>
-    <div>
-        <input type="submit" value="Login">
-    </div>
-    </form>
-    <br><br>
-    </div>
-    
-</center>
-</body>
-</html>
+
